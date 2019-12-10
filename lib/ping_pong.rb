05709912fbf9608e7a32cd4e887ffc70f1ea4afb
Lela Smith <smith.lelaa@gmail.com>
2019-12-09 22:21:03 -0800

    switch to each loop. pass second spec

--- lib/ping_pong.rb
+++ lib/ping_pong.rb
@@ -1,10 +1,30 @@
-# class Number
-  def ping_pong(num)
-    x = 0
-    output = []
-    while (x < num)
-      x = x + 1
-      output.push(x)
+def ping_pong(num)
+  output = []
+  (1..num).each do |i|
+    if i.%(3).eql?(0)
+      output.push("Ping")
+    else
+    output.push(i)
   end
-  output
 end
+output
+end
+
+
+# def ping_pong(num)
+#   x = 0
+#   output = []
+#   while (x < num)
+#     if (x % 3 == 0)
+#       x += 1
+#       output.push("Ping")
+#     # elsif x % 5 == 0
+#     #   x += 1
+#     #   p "Pong"
+#     else
+#       x += 1
+#        output.push(x)
+#     end
+#   end
+#   output
+# end
--- spec/ping_pong_spec.rb
+++ spec/ping_pong_spec.rb
@@ -2,7 +2,11 @@ require('rspec')
 require('ping_pong')
 
 describe('ping_pong') do
-  it("counts up to the number inputted") do
-    expect(ping_pong(3)).to(eq([1,2,3]))
+  # it("counts up to the number inputted") do
+  #   expect(ping_pong(3)).to(eq([1,2,3]))
+  # end
+
+  it("replaces numbers divisible by 3 with Ping") do
+    expect(ping_pong(3)).to(eq([1,2,"Ping"]))
   end
 end
